661127a97ea37cd9e7003eba2e77f49b50c7f4cb
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-07-01 14:16:24 -0400

    Initial Commit

---
+++ iSweepstakesAdmin/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  iSweepstakesAdmin
+//
+//  Created by Jack Borthwick on 7/1/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ iSweepstakesAdmin/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  iSweepstakesAdmin
+//
+//  Created by Jack Borthwick on 7/1/15.
+//  Copyright (c) 2015 Jack Borthwick. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
